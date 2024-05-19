a737612c1b7fd4396f924780dbe9392897bc4acd
Hanief Fathul B.A <hanieffathulb03@gmail.com>
2024-05-19 11:43:09 +0000

    feat: add consume main go hok-api

---
+++ api/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const axios = require("axios");
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+    const searchQuery = req.query.search;
+    const id = req.params.id;
+    
+    let Url = process.env.API_URL;
+
+    if (searchQuery) {
+        Url += `?search=${searchQuery}`;
+    } else if (id) {
+        Url += id;
+    }
+
+    try {
+        const response = await axios.get(Url);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get("/:id", async (req, res) => {
+    const id = req.params.id;
+    const Url = process.env.API_URL;
+    const apiUrl = `${Url}/${id}`;
+    
+    try {
+        const response = await axios.get(apiUrl);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
